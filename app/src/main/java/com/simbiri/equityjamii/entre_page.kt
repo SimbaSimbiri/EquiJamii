315d3579af5c2bd3068ae0620f815e1a68dd2ab0
SimbaSimbiri <105386979+SimbaSimbiri@users.noreply.github.com>
2023-06-05 16:06:13 -0500

    Added landscape mode xml

--- app/src/main/java/com/simbiri/equityjamii/entre_page.kt
+++ app/src/main/java/com/simbiri/equityjamii/entre_page.kt
@@ -1,5 +1,6 @@
 package com.simbiri.equityjamii
 
+import android.content.res.Configuration
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
@@ -29,7 +30,9 @@ class entre_page : AppCompatActivity() {
         setContentView(R.layout.activity_entre_page)
 
 
-        val image_list =  ArrayList<SlideModel>()
+
+
+       /* val image_list =  ArrayList<SlideModel>()
 
         image_list.add(SlideModel(R.drawable.europeanu_talks_image , "EU officials grace Equity Group Holdings", ScaleTypes.FIT))
         image_list.add(SlideModel(  R.drawable.jamesshakinghands, "Equity Group  registers  21% growth in total assets", ScaleTypes.FIT))
@@ -47,7 +50,7 @@ class entre_page : AppCompatActivity() {
         bottom_bar =  findViewById(R.id.bottom_app_bar)
         bottom_nav = findViewById(R.id.bottom_nav_view)
         fab = findViewById(R.id.floating_action_button)
-
+*/
         /*actionMenuView = findViewById(R.id.additional_menu)
 
         menuInflater.inflate(R.menu.bottom_menu, actionMenuView.menu)
@@ -56,7 +59,10 @@ class entre_page : AppCompatActivity() {
 
 
 
+
     }
+
+
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
 
@@ -65,5 +71,32 @@ class entre_page : AppCompatActivity() {
         return true
     }
 
+   /* override fun onConfigurationChanged(newConfig: Configuration) {
 
+        if (newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE){
+            setContentView(R.layout.activity_entre_page_landscape)
+
+            val image_list =  ArrayList<SlideModel>()
+
+            image_list.add(SlideModel(R.drawable.europeanu_talks_image , "EU officials grace Equity Group Holdings", ScaleTypes.FIT))
+            image_list.add(SlideModel(  R.drawable.jamesshakinghands, "Equity Group  registers  21% growth in total assets", ScaleTypes.FIT))
+            image_list.add(SlideModel(R.drawable.stanfordmba , "Stanford MBA influential visit to the bank", ScaleTypes.FIT))
+            image_list.add(SlideModel(R.drawable.elimu_talk, "Elimu Scholarship Program Day", ScaleTypes.FIT))
+            image_list.add(SlideModel(R.drawable.equity_assurance , "Equity maintains outstanding financial perfomance", ScaleTypes.FIT))
+            image_list.add(SlideModel(R.drawable.wings_to_fly_girls , "Wings to Fly Scholars fly abroad", ScaleTypes.FIT))
+
+
+            image_Slider = findViewById<ImageSlider>(R.id.image_slider)
+            image_Slider.setImageList(image_list)
+
+
+
+            bottom_bar =  findViewById(R.id.bottom_app_bar)
+            bottom_nav = findViewById(R.id.bottom_nav_view)
+
+        } else
+            setContentView(R.layout.activity_entre_page)
+        super.onConfigurationChanged(newConfig)
+    }
+*/
 }
