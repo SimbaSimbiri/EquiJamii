9eab2377e1f9038371c64200a99f210a67c52e39
SimbaSimbiri <simbiriisaacray@gmail.com>
2024-07-02 13:55:38 +0300

    text font to serif

--- app/src/main/java/com/simbiri/equityjamii/ui/main_activity/news_page/live_youtube/LiveVideosFragment.kt
+++ app/src/main/java/com/simbiri/equityjamii/ui/main_activity/news_page/live_youtube/LiveVideosFragment.kt
@@ -1,86 +1,107 @@
 package com.simbiri.equityjamii.ui.main_activity.news_page.live_youtube
 
+import android.content.Context
 import android.os.Bundle
-import android.os.Handler
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
-import androidx.cardview.widget.CardView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.denzcoskun.imageslider.ImageSlider
 import com.denzcoskun.imageslider.constants.ScaleTypes
 import com.denzcoskun.imageslider.models.SlideModel
-import com.simbiri.equityjamii.R
 import com.simbiri.equityjamii.adapters.LiveVideoAdapter
-import com.simbiri.equityjamii.data.model.YouTubeVids
-import kotlinx.coroutines.DelicateCoroutinesApi
+import com.simbiri.equityjamii.data.model.AuthUtils
+import com.simbiri.equityjamii.databinding.NewsPageLiveBinding
 import kotlinx.coroutines.launch
 
-@OptIn(DelicateCoroutinesApi::class)
 class LiveVideosFragment : Fragment() {
 
     companion object {
         fun newInstance() = LiveVideosFragment()
     }
 
+    private val viewModel = LiveVideosViewModel()
+    private val slideModels = arrayListOf<SlideModel>()
+    private lateinit var binding: NewsPageLiveBinding
 
-    private lateinit var viewModel: LiveVideosViewModel
-    private lateinit var recyclerVideos: RecyclerView
-    private lateinit var airingImageSlider: ImageSlider
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        lifecycleScope.launch {
+            viewModel.fetchVideos(context)
+        }
+    }
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
-    ): View? {
-        val view = inflater.inflate(R.layout.news_page_live, container, false)
-        airingImageSlider = view.findViewById(R.id.airingImageSwitcher)
-        recyclerVideos = view.findViewById(R.id.recyclerViewYoutubeLive)
+    ): View {
+        binding = NewsPageLiveBinding.inflate(layoutInflater)
 
+        if (AuthUtils.getCurrentUserId() != null) {
+            setUpObservers(requireContext())
+        }
         val layoutManager = LinearLayoutManager(context)
         layoutManager.orientation = RecyclerView.VERTICAL
-        recyclerVideos.layoutManager = layoutManager
+        binding.recyclerViewYoutubeLive.layoutManager = layoutManager
 
-        return view
+        return binding.root
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
+    private fun setUpObservers(context: Context) {
 
         lifecycleScope.launch {
-            val liveList = YouTubeVids.YoutubeVideos(requireContext(), "live")
-            val upcomingList = YouTubeVids.YoutubeVideos(requireContext(), "upcoming")
-            val completedList = YouTubeVids.YoutubeVideos(requireContext(), "completed")
-            val slideModels = arrayListOf<SlideModel>()
 
+            viewModel.liveList.observe(viewLifecycleOwner) { liveList ->
+                if (liveList.isNotEmpty()) {
+                    binding.currentTextV.visibility = View.VISIBLE
+                    binding.airingCurrentCardView.visibility = View.VISIBLE
 
-            if (liveList.isNotEmpty() && upcomingList.isNotEmpty()) {
-                val currentTv = view.findViewById<TextView>(R.id.currentTextV)
-                val currentCardVid = view.findViewById<CardView>(R.id.airingCurrentCardView)
-
-                currentTv.visibility = View.VISIBLE
-                currentCardVid.visibility = View.VISIBLE
-
-                liveList.forEach {video ->
-                    slideModels.add(
-                        SlideModel(
-                            video.thumbnailUrl, video.title, ScaleTypes.CENTER_CROP
+                    liveList.forEach { video ->
+                        slideModels.add(
+                            SlideModel(
+                                video.thumbnailUrl, video.title, ScaleTypes.CENTER_CROP
+                            )
                         )
-                    )
-                }
-                upcomingList.forEach { video ->
-                    slideModels.add(SlideModel(video.thumbnailUrl, video.title, ScaleTypes.CENTER_CROP))
+                    }
+
+                    binding.airingImageSwitcher.setImageList(slideModels)
+                    binding.progressBar.visibility = View.INVISIBLE
+
                 }
             }
 
-            airingImageSlider.setImageList(slideModels)
+            viewModel.upcomingList.observe(viewLifecycleOwner) { upcomingList ->
 
-            val adapter = context?.let { LiveVideoAdapter(it, completedList) }
-            recyclerVideos.adapter = adapter
-            recyclerVideos.adapter!!.notifyDataSetChanged()
+                if (upcomingList.isNotEmpty()) {
+                    binding.currentTextV.visibility = View.VISIBLE
+                    binding.airingCurrentCardView.visibility = View.VISIBLE
+
+                    upcomingList.forEach { video ->
+                        slideModels.add(
+                            SlideModel(
+                                video.thumbnailUrl,
+                                video.title,
+                                ScaleTypes.CENTER_CROP
+                            )
+                        )
+                    }
+
+                    binding.airingImageSwitcher.setImageList(slideModels)
+                    binding.progressBar.visibility = View.INVISIBLE
+
+                }
+            }
+
+            viewModel.completedList.observe(viewLifecycleOwner) { completedList ->
+                if (completedList.isNotEmpty()) {
+                    val adapterVideo = LiveVideoAdapter(context, completedList)
+                    binding.recyclerViewYoutubeLive.apply {
+                        adapter = adapterVideo
+                    }
+                    binding.recyclerViewYoutubeLive.adapter!!.notifyDataSetChanged()
+                    binding.progressBar.visibility = View.INVISIBLE
+                }
+            }
         }
     }
-
-
 }
--- app/src/main/java/com/simbiri/equityjamii/ui/main_activity/news_page/live_youtube/LiveVideosViewModel.kt
+++ app/src/main/java/com/simbiri/equityjamii/ui/main_activity/news_page/live_youtube/LiveVideosViewModel.kt
@@ -1,9 +1,32 @@
 package com.simbiri.equityjamii.ui.main_activity.news_page.live_youtube
 
+import android.content.Context
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.simbiri.equityjamii.data.model.Video
+import com.simbiri.equityjamii.data.model.YouTubeVids
+import kotlinx.coroutines.launch
 
 class LiveVideosViewModel : ViewModel() {
+
+    private val _liveList = MutableLiveData<List<Video>>()
+    val liveList: LiveData<List<Video>> = _liveList
+
+    private val _upcomingList = MutableLiveData<List<Video>>()
+    val upcomingList: LiveData<List<Video>> = _upcomingList
+
+
+    private val _completedList = MutableLiveData<List<Video>>()
+    val completedList: LiveData<List<Video>> = _completedList
+
+
+    suspend fun fetchVideos(context: Context) {
+        _liveList.value = YouTubeVids.YoutubeVideos(context, "live")
+        _upcomingList.value = YouTubeVids.YoutubeVideos(context, "upcoming")
+        _completedList.value = YouTubeVids.YoutubeVideos(context, "completed")
+    }
 
 }
 
