d82b477edb6a40a8097331cb3d31201580fb548f
SimbaSimbiri <simbiriisaacray@gmail.com>
2024-07-23 20:04:27 +0300

    prepared UI for task and mention

---
+++ app/src/main/java/com/simbiri/equityjamii/ui/main_activity/workspace_page/AddTaskFragment.kt
@@ -0,0 +1,31 @@
+package com.simbiri.equityjamii.ui.main_activity.workspace_page
+
+import androidx.fragment.app.viewModels
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.simbiri.equityjamii.R
+
+class AddTaskFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = AddTaskFragment()
+    }
+
+    private val viewModel: AddTaskViewModel by viewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // TODO: Use the ViewModel
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return inflater.inflate(R.layout.add_task_frag, container, false)
+    }
+}
